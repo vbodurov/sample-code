43fa2ef544925b5a1ae1b55208eddfeb7b40aa88
vbodurov <vladimir@bodurov.com>
2014-08-06 00:24:56 -0700

    median and heaps

---
+++ MiscCode/MiscCodeTests/MedianFromStreamOfNumbersTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using MiscCodeTests.Model;
+using NUnit.Framework;
+
+namespace MiscCodeTests
+{
+    [TestFixture]
+    public class MedianFromStreamOfNumbersTests
+    {
+        [Test]
+        public void CanGetMedianFromStreamOfIntegers()
+        {
+            var array =        new []{ 5, 15, 1, 3, 2, 8, 7, 9, 10, 6, 11, 4 };
+            var expectMedian = new[] { 5, 10, 5, 4, 3, 4, 5, 6,  7, 6,  7, 6 };
+            var median = array[0];
+
+            var left  = new MaxHeap();
+            var right = new MinHeap();
+
+            for (var i = 0; i < array.Length; ++i )
+            {
+                median = AddNumberAndGetMedian(array[i], median, left, right);
+                
+                Console.WriteLine(i + " => " + median);
+                Assert.That(median, Is.EqualTo(expectMedian[i]));
+            }
+        }
+
+        public int AddNumberAndGetMedian(int e, int prevMedian, BaseHeap left, BaseHeap right)
+        {
+            var leftCount = left.GetCount();
+            var rightCount = right.GetCount();
+
+            if(leftCount > rightCount)
+            {
+                if (e < prevMedian) 
+                {
+                    right.Insert(left.ExtractTop());
+
+                    left.Insert(e);
+                }
+                else
+                {
+                    right.Insert(e);
+                }
+
+                return Average(left.GetTop(), right.GetTop());
+            }
+
+            if (leftCount == rightCount)
+            {
+                if (e < prevMedian) 
+                {
+                    left.Insert(e);
+                    return left.GetTop();
+                }
+                right.Insert(e);
+                return right.GetTop();
+            }
+            
+            
+            // if(leftCount < rightCount) ...
+            
+            if (e < prevMedian)
+            {
+                left.Insert(e);
+            }
+            else
+            {
+                left.Insert(right.ExtractTop());
+
+                right.Insert(e);
+            }
+
+            return Average(left.GetTop(), right.GetTop());
+            
+        }
+
+        int Average(int a, int b) { return (a + b) / 2; }
+    }
+}
---
+++ MiscCode/MiscCodeTests/Model/BaseHeap.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MiscCodeTests.Model
+{
+    public class MaxHeap : BaseHeap { protected override bool Compare(int a, int b) { return a > b; } }
+    public class MinHeap : BaseHeap { protected override bool Compare(int a, int b) { return a < b; } }
+
+    public abstract class BaseHeap
+    {
+        private readonly List<int> _array = new List<int>();
+        private int _heapSize = -1;
+
+        protected abstract bool Compare(int a, int b);
+
+        public virtual void Insert(int key)
+        {
+            _heapSize++;
+            if (_array.Count <= _heapSize)
+            {
+                _array.AddRange(
+                    Enumerable.Range(0, _heapSize - (_array.Count - 1))
+                              .Select(i => 0));
+            }
+            _array[_heapSize] = key;
+            Heapify(_heapSize);
+        }
+        public virtual int GetTop()
+        {
+            int max = -1;
+
+            if (_heapSize >= 0)
+            {
+                max = _array[0];
+            }
+
+            return max;
+        }
+        public virtual int ExtractTop()
+        {
+            var del = -1;
+
+            if (_heapSize > -1)
+            {
+                del = _array[0];
+
+                Swap(_array, 0, _heapSize);
+                _heapSize--;
+                Heapify(GetParent(_heapSize + 1));
+            }
+
+            return del;
+        }
+        public virtual int GetCount()
+        {
+            return _heapSize + 1;
+        }
+        public override string ToString()
+        {
+            return "{" + string.Join(";", _array) + "}";
+        }
+
+
+        private static void Swap(IList<int> array, int aIndex, int bIndex )
+        {
+            var aux = array[aIndex];
+            array[aIndex] = array[bIndex];
+            array[bIndex] = aux;
+        }
+
+        protected int left(int i)
+        {
+            return 2 * i + 1;
+        }
+ 
+        protected int right(int i)
+        {
+            return 2 * (i + 1);
+        }
+ 
+        protected int GetParent(int i)
+        {
+            if( i <= 0 )
+            {
+                return -1;
+            }
+ 
+            return (i - 1)/2;
+        }
+ 
+        protected void Heapify(int i)
+        {
+            int p = GetParent(i);
+ 
+            if( p >= 0 && Compare(_array[i], _array[p]) )
+            {
+                Swap(_array, i, p);
+                Heapify(p);
+            }
+        }
+
+    }
+}
