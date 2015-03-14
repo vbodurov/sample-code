87447ed066a25b02955df840bc1f94bdc9c90997
vbodurov <vladimir@bodurov.com>
2015-03-14 04:01:24 -0700

    unity tests updated

--- MiscCode/MiscCodeTests/UnityTests.cs
+++ MiscCode/MiscCodeTests/UnityTests.cs
@@ -24,27 +24,34 @@ namespace MiscCodeTests
             container.BuildUp(typeof (OneAttribute), attr0);
             container.BuildUp(typeof (OneSubAttribute), attr1);
 
-            Console.WriteLine("0:"+attr0.Prop1.GetType());
-            Console.WriteLine("1:"+attr1.Prop1.GetType());
+            Console.WriteLine("0:"+attr0.GetPropType());
+            Console.WriteLine("1:"+attr1.GetPropType());
 
-            Assert.That(attr0.Prop1.GetType(), Is.EqualTo(typeof(Class1)));
-            Assert.That(attr1.Prop1.GetType(), Is.EqualTo(typeof(Class2)));
+            Assert.That(attr0.GetPropType(), Is.EqualTo(typeof(Class1)));
+            Assert.That(attr1.GetPropType(), Is.EqualTo(typeof(Class2)));
         }
         
-    }
-    [AttributeUsage(AttributeTargets.All)]
-    public class OneSubAttribute : OneAttribute
-    {
-        [Dependency("ALT")]
-        internal new IClass Prop1 { get; set; }
     }
     public interface IClass { }
     public class Class1 : IClass { }
     public class Class2 : IClass { }
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class OneSubAttribute : OneAttribute
+    {
+        [Dependency("ALT")]
+        internal IClass SubProp1 { get; set; }
+        internal override IClass Prop1 { get { return SubProp1; } set{} }
+    }
     [AttributeUsage(AttributeTargets.All)]
     public class OneAttribute : Attribute
     {
         [Dependency]
-        internal IClass Prop1 { get; set; }
+        internal virtual IClass Prop1 { get; set; }
+
+        public Type GetPropType()
+        {
+            return Prop1.GetType();
+        }
     }
 }
