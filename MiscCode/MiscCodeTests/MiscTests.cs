93a9ae56539e2dc145785aec16bed4f6124e156e
vbodurov <vladimir@bodurov.com>
2015-03-15 23:10:40 -0700

    misc

--- MiscCode/MiscCodeTests/MiscTests.cs
+++ MiscCode/MiscCodeTests/MiscTests.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Practices.ObjectBuilder2;
 using NUnit.Framework;
 
 namespace MiscCodeTests
@@ -9,7 +12,21 @@ namespace MiscCodeTests
         [Test]
         public void Test1()
         {
-            Console.WriteLine(((int)"9".ToLowerInvariant()[0] - 'a'));
+            var s = "/Users/vladimirbodurov/Desktop/Code/GitHub/multiplayer-experiment/CircuitRacerBegin/Assets";
+            //s:/Users/vladimirbodurov/Desktop/Code/GitHub/multiplayer-experiment/CircuitRacerBegin/Assets
+
+            var arr = s.Split(new[] {'/', '\\'}, StringSplitOptions.None);
+
+            //code:/Users/vladimirbodurov/Desktop/Code/
+            var codeRoot = String.Join("/", arr.Take(arr.Length - 4))+"/";
+
+            //gitRoot:/Users/vladimirbodurov/Desktop/Code/GitHub/multiplayer-experiment/
+            var gitRoot = String.Join("/", arr.Take(arr.Length - 2))+"/";
+
+            //unityRoot:/Users/vladimirbodurov/Desktop/Code/GitHub/multiplayer-experiment/CircuitRacerBegin/
+            var unityRoot = String.Join("/", arr.Take(arr.Length - 1))+"/";
+
+            Console.WriteLine(unityRoot);
         }
         [Test]
         public void Test2()
